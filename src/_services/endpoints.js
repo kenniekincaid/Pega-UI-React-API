d6fc824c0aff9af991a8d0b7740396fb98a9cfaa
Kendra Roberson <kendra.roberson@verizon.com>
2019-12-19 14:57:06 -0600

    Added new Base URL to use Personal Pega 8
    Also installed yard.

--- src/_services/endpoints.js
+++ src/_services/endpoints.js
@@ -1,7 +1,8 @@
 export const endpoints = {
   // Change this URL if you want to point the React application at another Pega server.
   // BASEURL: "http://10.60.213.113:9080/prweb/api/v1",
-  BASEURL: "https://rtd-dev1.ebiz.verizon.com:443/etr/api/v1/",
+  BASEURL: "http://localhost:8080/prweb/api/v1",
+  // BASEURL: "https://rtd-dev1.ebiz.verizon.com:443/etr/api/v1/",
 
   AUTH: "/authenticate",
   CASES: "/cases",
